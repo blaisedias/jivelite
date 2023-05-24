2d4d5fa32956c5b7a784ddea23af5c4a66cb9933
Blaise Dias <blaise.dias@datacore.com>
2023-05-25 00:56:55 +0100

    chore: mini spectrum now playing screen
    see https://forums.slimdevices.com/forum/user-forums/linux-unix/1638718-adding-a-new-nowplaying-style-help
    Changes based on that discussion

--- share/jive/applets/NowPlaying/NowPlayingApplet.lua
+++ share/jive/applets/NowPlaying/NowPlayingApplet.lua
@@ -1569,6 +1569,20 @@ function _createUI(self)
 		)
 	end
 
+	-- Visualizer: Mini Spectrum Visualizer - only load if needed
+	if self.windowStyle == "nowplaying_minispectrum_text" then
+		self.visuGroup = Button(
+			Group('npvisu', {
+				visu = SpectrumMeter("spectrum"),
+			}),
+			function()
+				Framework:pushAction("go_now_playing")
+				return EVENT_CONSUME
+			end
+		)
+	end
+
+
 	-- Visualizer: Analog VU Meter - only load if needed
 	if self.windowStyle == "nowplaying_vuanalog_text" then
 		self.visuGroup = Button(
@@ -1738,7 +1752,7 @@ function _createUI(self)
 	window:addWidget(self.artistalbumTitle)
 	window:addWidget(self.artworkGroup)
 	-- Visualizer: Only load if needed
-	if (self.windowStyle == "nowplaying_spectrum_text") or (self.windowStyle == "nowplaying_vuanalog_text") then
+	if (self.windowStyle == "nowplaying_spectrum_text") or (self.windowStyle == "nowplaying_vuanalog_text") or (self.windowStyle == "nowplaying_minispectrum_text")  then
 		window:addWidget(self.visuGroup)
 	end
 
