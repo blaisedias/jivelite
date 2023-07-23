c35ea5117f59e0aa12c517807926372656d75f63
Blaise Dias <blaise.dias@datacore.com>
2023-07-23 22:13:55 +0100

    chore: streamline new VUMeter rendering code pt-1
    refactor to simplify and isolate context for vumeter types
    part 1 - fix new vumeter rendering.

--- share/jive/jive/vis/VUMeter.lua
+++ share/jive/jive/vis/VUMeter.lua
@@ -103,53 +103,43 @@ function _layout(self)
 				if self.bgImg ~= nil then
 					local imgW, imgH = self.bgImg:getSize()
 					-- FIXME VU Meter images will not always be 25 frames
-					self.frame_w = imgW/25
+					local frame_w = imgW/25
 					-- centre the VUMeter image within the designated space
 					-- horizontally
-					self.x1 = x + self.w - self.frame_w
-					self.x2 = self.x1 + self.frame_w
+					local fx= x + math.floor(w / 2) - frame_w
 					-- vertically
+					local fy = y
+					local src_y = 0
 					if imgH < h then
-						self.y = math.floor(self.y + ((h - imgH)/2))
+						fy = math.floor(self.y + ((h - imgH)/2))
 					elseif imgH > h then
 						-- clip the image at the top and bottom
-						self.src_y = math.floor((imgH - h)/2)
+						src_y = math.floor((imgH - h)/2)
 					end
-					log:debug("** x1:", self.x1, " x2:", self.x2, " y:", self.y, " src_y:", self.src_y)
-					log:debug("** w:", self.w, " frame_w:", self.frame_w, " h:", self.h)
-					log:debug("** bgImg-w:", imgW, " bgImg-h:", imgH)
+					self.left =  { img=self.bgImg, x=fx ,                   y=fy, src_y=src_y, w=frame_w, h=imgH, cap=0}
+					self.right = { img=self.bgImg, x=self.left.x + frame_w, y=fy, src_y=src_y, w=frame_w, h=imgH, cap=0}
+					log:debug("left : x:", self.left.x, " y:", self.left.y, " src_y:", self.left.src_y, " w:", self.left.w, " h:", self.left.h)
+					log:debug("right: x:", self.right.x, " y:", self.right.y, " src_y:", self.right.src_y, " w:", self.right.w, " h:", self.right.h)
+					self.drawMeter = draw25FrameVuMeter
+				else
+					self.left =  { img=bgImg }
+					self.right = { img=bgImg }
+				    self.drawMeter = nullDraw
 				end
 			elseif self.vutype == "vfd" then
 				self.vfd = self.vutbl
-				self.x1 = x + math.floor((w - self.vfd.w)/2)
-				self.x2 = self.x1
---				log:debug("******* vfd x=", self.x1)
+				local vfdx = x + math.floor((w - self.vfd.w)/2)
 				local y1 = self.y + math.floor((h - self.vfd.h)/2)
---				log:debug("******* vfd y1=", y1)
-				self.vfd.cy = y1 + self.vfd.bh
-				local y2 = self.vfd.cy + self.vfd.ch
---				log:debug("******* vfd y2=", y2)
-				self.vfd.y = {y1, y2}
-				self.vfd.cap = {-1, -1}
-				self.vfd.peak_hold_counter = {0, 0}
---				log:debug("******* vfd y1=", y1, " cy=", self.vfd.cy, " y2=", y2)
+				local y2 = y1 + self.vfd.bh + self.vfd.ch
+				self.left =  {x=(vfdx + self.vfd.lw), y=y1, cap=0, peak_hold_counter=0, vfd=self.vfd}
+				self.right = {x=(vfdx + self.vfd.lw), y=y2, cap=0, peak_hold_counter=0, vfd=self.vfd}
+				self.drawMeter = drawVFD
+
+				self.bgParams = {x=vfdx, y=y1 + self.vfd.bh, vfd=self.vfd, left=self.left, right=self.right}
+				self.drawBackground = drawVFDStatic
 			end
 		end
 	end
-end
-
-
-function draw(self, surface)
-	if self.style == "vumeter" then
-		self.bgImg:blit(surface, self:getBounds())
-	end
-
-	local sampleAcc = vis:vumeter()
-
-	-- local volume = self.player:getVolume()
-
-	_drawMeter(self, surface, sampleAcc, 1, self.x1, self.y, self.w, self.h)
-	_drawMeter(self, surface, sampleAcc, 2, self.x2, self.y, self.w, self.h)
 end
 
 -- FIXME dynamic based on number of bars
@@ -161,18 +151,41 @@ local RMS_MAP = {
 	3227, 3414, 3601, 3788, 3975, 4162, 4349, 4536, 4755,
 }
 
-function _drawMeter(self, surface, sampleAcc, ch, x, y, w, h)
-	local val = 1
+function samplAcc2Vol(sampleAcc)
 	for i = #RMS_MAP, 1, -1 do
-		if sampleAcc[ch] > RMS_MAP[i] then
-			val = i
-			break
-		end
+ 		if sampleAcc > RMS_MAP[i] then
+ 			return i
+  		end
+   	end
+	return 1
+end
+
+function draw(self, surface)
+	if self.style == "vumeter" then
+		self.bgImg:blit(surface, self:getBounds())
 	end
 
-	local vol = val
+	if self.bgParams ~= nil then
+		self.drawBackground(self.bgParams, surface)
+	end
+
+	local sampleAcc = vis:vumeter()
+	local vol = {samplAcc2Vol(sampleAcc[1]), samplAcc2Vol(sampleAcc[2])}
+
+	-- local volume = self.player:getVolume()
+
+	if self.drawMeter == nil then
+		_drawMeter(self, surface, vol[1], 1, self.x1, self.y, self.w, self.h)
+		_drawMeter(self, surface, vol[2], 2, self.x2, self.y, self.w, self.h)
+	else
+		self.drawMeter(self.left, surface, vol[1]) 
+		self.drawMeter(self.right, surface, vol[1]) 
+	end
+end
+
+function _drawMeter(self, surface, vol, ch, x, y, w, h, ptr)
 	-- FIXME when rms map scaled
-	val = math.floor((val % 49)/2)
+	local val = math.min(math.floor(vol/2), 24)
 
 --	val = math.floor(math.log(sampleAcc[ch]) * 1.5)
 --	if val > 24 then
@@ -205,52 +218,59 @@ function _drawMeter(self, surface, sampleAcc, ch, x, y, w, h)
 
 --		local x,y,w,h = self:getBounds()
 
-		if self.legacy then
-			self.bgImg:blitClip(self.cap[ch] * self.w, y, self.frame_w, h, surface, x, y)
-		elseif self.vutype == "frame" then
-			if self.bgImg ~= nil then
-				self.bgImg:blitClip(self.cap[ch] * self.frame_w, self.src_y, self.frame_w, h, surface, x, y)
-			end
-		elseif self.vutype == "vfd" then
-			if vol > 1 and vol >= self.vfd.cap[ch] then
-				self.vfd.cap[ch] = vol
-				self.vfd.peak_hold_counter[ch] = math.floor(FRAME_RATE/2)
-			else
-				self.vfd.peak_hold_counter[ch] = self.vfd.peak_hold_counter[ch] - 1
-				if self.vfd.peak_hold_counter[ch] < 1 then
-					self.vfd.cap[ch] = -1
-				end
-			end
-
-			local dvx = x
-			local dvy = self.vfd.y[ch]
-			self.vfd.blead[ch]:blit(surface, dvx, dvy, self.vfd.lw, self.vfd.lh)
-			dvx = dvx + self.vfd.lw
-			for i = 1, 36 do
-				if i < vol or i == self.vfd.cap[ch] then
-					self.vfd.on:blit(surface, dvx, dvy, self.vfd.bw, self.vfd.bh)
-				else
-					self.vfd.off:blit(surface, dvx, dvy, self.vfd.bw, self.vfd.bh)
-				end
-				dvx = dvx + self.vfd.bw
-			end
-			for i = 37, 49 do
-				if i < vol or i == self.vfd.cap[ch] then
-					self.vfd.peakon:blit(surface, dvx, dvy, self.vfd.bw, self.vfd.h)
-				else
-					self.vfd.peakoff:blit(surface, dvx, dvy, self.vfd.bw, self.vfd.h)
-				end
-				dvx = dvx + self.vfd.bw
-			end
-
-			if self.vfd.center ~= nil then
-				self.vfd.center:blit(surface, x, self.vfd.cy, self.vfd.cw, self.vfd.ch)
-			end
-
-		end
+		self.bgImg:blitClip(self.cap[ch] * self.w, y, self.frame_w, h, surface, x, y)
 	end
 end
 
+function drawVFD(self, surface, vol)
+	local x = self.x
+	local y = self.y
+	if vol >= self.cap then
+		self.cap = vol
+		self.peak_hold_counter = math.floor(FRAME_RATE/2)
+	else
+		self.peak_hold_counter = self.peak_hold_counter - 1
+		if self.peak_hold_counter < 1 then
+			self.cap = 0
+		end
+	end
+	for i = 2, 37 do
+		if i <= vol or i == self.cap then
+			self.vfd.on:blit(surface, x, y, self.vfd.bw, self.vfd.bh)
+		else
+			self.vfd.off:blit(surface, x, y, self.vfd.bw, self.vfd.bh)
+		end
+		x = x + self.vfd.bw
+	end
+	for i = 38, 50 do
+		if i <= vol or i == self.cap then
+			self.vfd.peakon:blit(surface, x, y, self.vfd.bw, self.vfd.h)
+		else
+			self.vfd.peakoff:blit(surface, x, y, self.vfd.bw, self.vfd.h)
+		end
+		x = x + self.vfd.bw
+	end
+end
+
+function drawVFDStatic(tbl, surface) 
+	tbl.vfd.center:blit(surface, tbl.x, tbl.y, tbl.w, tbl.h)
+	tbl.vfd.blead[1]:blit(surface, tbl.x, tbl.left.y,  tbl.vfd.lw, tbl.vfd.lh)
+	tbl.vfd.blead[2]:blit(surface, tbl.x, tbl.right.y, tbl.vfd.lw, tbl.vfd.lh)
+end
+
+
+function draw25FrameVuMeter(self, surface, vol)
+	local val = math.min(math.floor(vol/2), 24)
+	if val >= self.cap then
+		self.cap = val
+	elseif self.cap > 0 then
+		self.cap = self.cap -1
+	end
+	self.img:blitClip(self.cap * self.w, self.src_y, self.w, self.h, surface, self.x, self.y)
+end
+
+function nullDraw(self, surface)
+end
 
 --[[
 
