ee4e2315a770214a11d7edce7cf3c76f12c4f018
Blaise Dias <blaise.dias@datacore.com>
2023-11-17 21:09:19 +0000

    chore: add support for 64-bit download
    Add mandatory option to select 32 or 64 bit download
    to daab-dl-drive.sh

--- scripts/daab-dl-drive.sh
+++ scripts/daab-dl-drive.sh
@@ -11,16 +11,27 @@ function dl {
 }
 
 
-if ! command -v curl &> /dev/null ; then
-    echo "please install curl with command \"tce-load -wi curl\""
-else
-    # 23/10/2023
-    if [ -d '../tce/optional' ] ; then
-        cd '../tce/optional'
-        dl 1uymZv-3JrEAjP-vJi7I2kV9RQQIKngfA "pcp-jivelite_hdskins.tcz"
-        dl 1FPkbZ9vb-OhQSc9So-NKrSkQOKsdizai "pcp-jivelite_hdskins.tcz.md5.txt"
-        dl 1cW_EMvnsjQzpGG9DwoAUBVhpFdMHefJX "pcp-jivelite.tcz"
-        dl 17JRIndz3oBKUKqjcpeQt4Wj3nUyfOEFp "pcp-jivelite.tcz.md5.txt"
+if [ "$1" == "32" ] || [ "$1" == "64" ]; then
+    if ! command -v curl &> /dev/null ; then
+        echo "please install curl with command \"tce-load -wi curl\""
+    else
+        # 23/10/2023
+        if [ -d '../tce/optional' ] ; then
+            cd '../tce/optional'
+            dl 1uymZv-3JrEAjP-vJi7I2kV9RQQIKngfA "pcp-jivelite_hdskins.tcz"
+            dl 1FPkbZ9vb-OhQSc9So-NKrSkQOKsdizai "pcp-jivelite_hdskins.tcz.md5.txt"
+            if [ "$1" == "32" ]; then
+                echo "32-bit"
+                dl 1cW_EMvnsjQzpGG9DwoAUBVhpFdMHefJX "pcp-jivelite.tcz"
+                dl 17JRIndz3oBKUKqjcpeQt4Wj3nUyfOEFp "pcp-jivelite.tcz.md5.txt"
+            else
+                echo "64-bit"
+                dl 1TvB0pRxvJRcUcWfb6smeS8hfeaK-exNB "pcp-jivelite.tcz"
+                dl 1fIq8dA3HHcF6qOiZTf-ydXiZoXouVEVp "pcp-jivelite.tcz.md5.txt"
+            fi
+        fi
     fi
+else
+    echo "usage $0 <32|64>"
 fi
 
