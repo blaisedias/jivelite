e0c93b460d47f3159a1153e3ff747bdcf0d222b9
Blaise Dias <blaise.dias@datacore.com>
2024-02-29 00:13:05 +0000

    chore: Visualiser applet

---
+++ share/jive/applets/Visualiser/VisualiserMeta.lua
@@ -0,0 +1,80 @@
+
+--[[
+=head1 NAME
+
+applets.Visualiser.VisualiserMeta - Visualiser meta-info
+
+=head1 DESCRIPTION
+
+See L<applets.Visualiser.VisualiserApplet>.
+
+=head1 FUNCTIONS
+
+See L<jive.AppletMeta> for a description of standard applet meta functions.
+
+=cut
+--]]
+
+local bit = bit
+
+local oo            = require("loop.simple")
+
+local AppletMeta    = require("jive.AppletMeta")
+
+local Framework     = require("jive.ui.Framework")
+
+local appletManager = appletManager
+local jiveMain      = jiveMain
+local Checkbox      = require("jive.ui.Checkbox")
+local visImage      = require("jive.visImage")
+local os            = require("os")
+
+local arg, ipairs, string = arg, ipairs, string
+
+module(...)
+oo.class(_M, AppletMeta)
+
+
+function jiveVersion(self)
+	return 1, 1
+end
+
+
+function registerApplet(self)
+end
+
+function configureApplet(self)
+
+--	self.vic_checkbox = Checkbox("checkbox", function(applet, checked)
+--		visImage:setCacheEnabled(checked)
+--        local popup = jive.ui.Popup("waiting_popup", "Boo1")
+--        popup:show()
+--        os.sleep(5)
+--        popup:hide()
+--	end)
+--	jiveMain:addItem({
+--		id = "vic",
+--		node = "settings",
+--		text = self:string('VISUALISER_IMAGE_CACHING'),
+--		style = 'item_choice',
+--		check = self.vic_checkbox,
+--	})
+--	self.vic_checkbox:setSelected(visImage:getCacheEnabled())
+   local icon = 'hm_settings'
+    
+
+	-- we only register the menu her, as registerApplet is being called before the skin is initialized
+    jiveMain:addItem(
+    	self:menuItem(
+    		'VisualiserApplet',
+    		'settings',
+    		'Visualiser',
+    		function(applet, ...) 
+    			applet:menu(...)
+    		end,
+    		100,
+    		nil,
+		icon
+    	)
+    )
+end
