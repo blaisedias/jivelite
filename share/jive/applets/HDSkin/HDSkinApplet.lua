4d25cd51ec3a19c713adb1817f3473cc375a081e
Blaise Dias <blaise.dias@datacore.com>
2024-05-05 21:09:24 +0100

    chore: add pcp setings icon to HD skins

--- share/jive/applets/HDSkin/HDSkinApplet.lua
+++ share/jive/applets/HDSkin/HDSkinApplet.lua
@@ -2472,6 +2472,9 @@ function skin(self, s)
 	s.hm_settings = _uses(_buttonicon, {
 		img = _loadImage(self, "IconsResized/icon_settings" .. skinSuffix),
 	})
+	s.hm_settings_pcp = _uses(_buttonicon, {
+		img = _loadImage(self, "IconsResized/icon_settings_pcp" .. skinSuffix),
+	})
 	s.hm_advancedSettings = _uses(_buttonicon, {
 		img = _loadImage(self, "IconsResized/icon_settings_adv" .. skinSuffix),
 	})
