5b9f95c9825cab532cc72ed38eebd9313bf9ab00
Blaise Dias <blaise.dias@datacore.com>
2024-06-29 12:22:29 +0100

    fix: more linter based fixes
    - add .luacheckrc to add global definitions
    - rationalise spectrum to visimage interface
    - reorder so that local fucntions are defined before invocation
      in the code

--- share/jive/jive/vis/SpectrumMeter.lua
+++ share/jive/jive/vis/SpectrumMeter.lua
@@ -1,20 +1,21 @@
 local oo            = require("loop.simple")
 local math          = require("math")
 
-local Framework     = require("jive.ui.Framework")
+--local Framework     = require("jive.ui.Framework")
 local Icon          = require("jive.ui.Icon")
-local Surface       = require("jive.ui.Surface")
-local Timer         = require("jive.ui.Timer")
-local Widget        = require("jive.ui.Widget")
+--local Surface       = require("jive.ui.Surface")
+--local Timer         = require("jive.ui.Timer")
+--local Widget        = require("jive.ui.Widget")
 
 local vis           = require("jive.vis")
 local visImage      = require("jive.visImage")
 
-local debug         = require("jive.utils.debug")
+--local debug         = require("jive.utils.debug")
 local log           = require("jive.utils.log").logger("jivelite.vis")
 
 local FRAME_RATE    = jive.ui.FRAME_RATE
-local type	= type
+--local type	= type
+local string        = require("jive.utils.string")
 local sep = 6
 
 module(...)
@@ -46,9 +47,6 @@ function _skin(self)
 	self.barColor = self:styleColor("barColor", { 0xff, 0xff, 0xff, 0xff })
 
 	self.capColor = self:styleColor("capColor", { 0xff, 0xff, 0xff, 0xff })
-
-	self.useVisImage = self:styleValue("useVisImage", false)
-	self.spType = self:styleValue("spType")
 end
 
 
@@ -61,11 +59,7 @@ function _layout(self)
 		return
 	end
 
-	self.capHeight = {}
-	self.capSpace = {}
-
 	self.channelWidth = {}
-	self.channelFlipped = {}
 	self.barsInBin = {}
 	self.barWidth = {}
 	self.barSpace = {}
@@ -76,32 +70,16 @@ function _layout(self)
 
 	self.isMono =  self:styleValue("isMono")
 
-	if self.useVisImage then
-		self.fgImg, self.bgImg, self.bgAlpha, barColour, capColour = visImage:getSpectrum(w, h, self.spType)
-		self.turbine = visImage.getSpectrumTurbine()
-		log:debug("barColour=", barColour, " ", type(barColour), " capColour=", capColour, " ", type(capColour), " turbine=", self.turbine)
-		if type(barColour) == "number" then
-			self.barColor = barColour
-		end
-		if type(capColour) == "number" then
-			self.capColor = capColour
-		end
-	end
+	self.fgImg, self.bgImg, self.bgAlpha, self.barColor, self.capColor = visImage:getSpectrum(w, h, self.barColor, self.capColor)
+	log:info('** barColor=', string.format("0x%x", self.barColor), " capColor=", string.format("0x%x",self.capColor))
+	self.settings = visImage:getSpectrumMeterSettings(self:styleValue("capHeight"), self:styleValue("capSpace"))
 
---	self.capHeight = self:styleValue("capHeight")
---	self.capSpace = self:styleValue("capSpace")
-	self.capHeight, self.capSpace = visImage:getCapsValues(self:styleValue("capHeight"), self:styleValue("capSpace"))
-	self.spBaselineAlways, self.spBaselineOn = visImage:getBaselineValues()
---	self.channelFlipped = self:styleValue("channelFlipped")
-	self.channelFlipped = visImage:getChannelFlipValues()
 	self.clipSubbands = self:styleValue("clipSubbands")
-	-- selectable from ui
-	local barsFormat = visImage:getBarsFormat()
-	self.barsInBin = {barsFormat.barsInBin, barsFormat.barsInBin}
-	self.barWidth = {barsFormat.barWidth, barsFormat.barWidth}
-	self.barSpace = {barsFormat.barSpace, barsFormat.barSpace}
-	self.binSpace = {barsFormat.binSpace, barsFormat.binSpace}
-	
+	self.barsInBin = {self.settings.barsFormat.barsInBin, self.settings.barsFormat.barsInBin}
+	self.barWidth = {self.settings.barsFormat.barWidth, self.settings.barsFormat.barWidth}
+	self.barSpace = {self.settings.barsFormat.barSpace, self.settings.barsFormat.barSpace}
+	self.binSpace = {self.settings.barsFormat.binSpace, self.settings.barsFormat.binSpace}
+
 	self.backgroundDrawn = false;
 
 	if self.barsInBin[1] < 1 then
@@ -125,18 +103,16 @@ function _layout(self)
 	self.channelWidth[1] = ((w - l - r) / 2) - (sep - self.binSpace[1])
 	self.channelWidth[2] = ((w - l - r) / 2) - (sep - self.binSpace[2])
 
-	local numBars = {}
-
-	numBars = vis:spectrum_init(
+	local numBars = vis:spectrum_init(
 		self.isMono,
 
 		self.channelWidth[1],
-		self.channelFlipped[1],
+		self.settings.channelFlipped[1],
 		barSize[1],
 		self.clipSubbands[1],
 
 		self.channelWidth[2],
-		self.channelFlipped[2],
+		self.settings.channelFlipped[2],
 		barSize[2],
 		self.clipSubbands[2]
 	)
@@ -146,8 +122,8 @@ function _layout(self)
 
 	local barHeight = {}
 
-	barHeight[1] = h - t - b - self.capHeight[1] - self.capSpace[1]
-	barHeight[2] = h - t - b - self.capHeight[2] - self.capSpace[2]
+	barHeight[1] = h - t - b - self.settings.capHeight[1] - self.settings.capSpace[1]
+	barHeight[2] = h - t - b - self.settings.capHeight[2] - self.settings.capSpace[2]
 
 	-- max bin value from C code is 31
 	self.barHeightMulti = {}
@@ -155,7 +131,7 @@ function _layout(self)
 	self.barHeightMulti[2] = barHeight[2] / 31
 
 	self.xshift = x + l
-	self.x1 = x + l + self.channelWidth[1] - numBars[1] * barSize[1] + 2 
+	self.x1 = x + l + self.channelWidth[1] - numBars[1] * barSize[1] + 2
 	self.x2 = x + l + self.channelWidth[2] + self.binSpace[2] + (sep - self.binSpace[1]) + (sep - self.binSpace[2])
 	self.xSpan =  numBars[1] * barSize[1] - self.binSpace[1] - 1
 
@@ -172,8 +148,8 @@ function _layout(self)
 	self.yCT = self.y - (self.h/2)
 	self.fgimg_yoffset = 0
 	if self.nplarge then
-	    self.y = y + h
-	    self.yCT = self.y - (self.h/2)
+		self.y = y + h
+		self.yCT = self.y - (self.h/2)
 		self.fgimg_yoffset = t
 	end
 
@@ -188,78 +164,7 @@ function _layout(self)
 
 end
 
-
-function draw(self, surface)
--- Black background instead of image
---	self.bgImg:blit(surface, self:getBounds())
-	local x, y, w, h = self:getBounds()
-
-	-- Avoid calling this more than once as it's not necessary
-	if not self.backgroundDrawn then
-		surface:filledRectangle(x, y, x + w, y + h, self.bgCol)
-		self.backgroundDrawn = true
-	end
-	if self.bgImg ~= nil then
---		self.bgImg:blit(surface, x, y, w, h, 0, 0)
---		self.bgImg:blitClip(0, 0, w, h, surface, x, y)
---		self.bgImg:blitAlpha(surface, x, y, self.bgAlpha)
-		self.bgImg:blit(surface, x, y)
-	end
-
-	local bins = { {}, {} }
-
-	bins[1], bins[2] = vis:spectrum()
-
-	local nz1 = false
-	local nz2 = false
-	nz1 = _drawBins(
-		self, surface, bins, 1, self.x1, self.y, self.barsInBin[1],
-		self.barWidth[1], self.barSpace[1], self.binSpace[1],
-		self.barHeightMulti[1], self.capHeight[1], self.capSpace[1]
-	)
-	nz2 = _drawBins(
-		self, surface, bins, 2, self.x2, self.y, self.barsInBin[2],
-		self.barWidth[2], self.barSpace[2], self.binSpace[2],
-		self.barHeightMulti[2], self.capHeight[2], self.capSpace[2]
-	)
-
-	-- simulate draw analyzer baseline only if playing,
-	-- by not drawing baseline if volume is 0
-	-- good enough
-	if self.spBaselineAlways or ((nz1 or nz2) and self.spBaselineOn) then
-		if self.fgImg ~= nil then
-			if self.turbine then
-				self.fgImg:blitClip(self.x1 - x, (h/2) - self.halfblH, self.xSpan, self.blH, surface, self.x1, self.yCT - self.halfblH)
-				self.fgImg:blitClip(self.x2 - x, (h/2) - self.halfblH, self.xSpan, self.blH, surface, self.x2, self.yCT - self.halfblH)
-			else
-				self.fgImg:blitClip(self.x1 - x, h - self.fgimg_yoffset - self.blH, self.xSpan, self.blH, surface, self.x1, self.y - self.fgimg_yoffset - self.blH)
-				self.fgImg:blitClip(self.x2 - x, h - self.fgimg_yoffset - self.blH, self.xSpan, self.blH, surface, self.x2, self.y - self.fgimg_yoffset - self.blH)
-			end
-		else
-			if self.turbine then
-				surface:filledRectangle(self.x1, self.yCT - self.halfblH, self.x1 + self.xSpan , self.yCT + self.blH - self.halfblH, self.barColor)
-				surface:filledRectangle(self.x2, self.yCT - self.halfblH, self.x2 + self.xSpan , self.yCT + self.blH - self.halfblH, self.barColor)
-			else
-				surface:filledRectangle(self.x1, self.y - self.blH - self.fgimg_yoffset, self.x1 + self.xSpan, self.y - self.fgimg_yoffset, self.barColor)
-				surface:filledRectangle(self.x2, self.y - self.blH - self.fgimg_yoffset, self.x2 + self.xSpan, self.y - self.fgimg_yoffset, self.barColor)
-			end
-		end
-	end
-	if self.countDown then
-		self.counter = self.counter - 1
-		if self.counter < 1 then
-			visImage:spChange('force', 'force')
-			self:_layout()
-		end
-	end
-end
-
-function twiddle(self)
-	visImage:spChange('force', 'force')
-	self:_layout()
-end
-
-function _drawBins(self, surface, bins, ch, x, y_in, barsInBin, barWidth, barSpace, binSpace, barHeightMulti, capHeight, capSpace)
+local function _drawBins(self, surface, bins, ch, x, y_in, barsInBin, barWidth, barSpace, binSpace, barHeightMulti, capHeight, capSpace)
 	local bch = bins[ch]
 	local cch = self.cap[ch]
 	local barSize = barWidth + barSpace
@@ -286,20 +191,22 @@ function _drawBins(self, surface, bins, ch, x, y_in, barsInBin, barWidth, barSpa
 			for k = 0, barsInBin - 1 do
 				if self.fgImg ~= nil then
 					local xLeft = x + (k * barSize)
-					if self.turbine then
+					if self.settings.turbine then
 						local yTop = self.yCT - (bch[i]/2)
 						self.fgImg:blitClip(xLeft - xshift, (hh/2) - (bch[i]/2), barWidth, bch[i], surface, xLeft, yTop)
 					else
 						local yTop = y - bch[i] + 1
 						self.fgImg:blitClip(xLeft - xshift, hh - self.fgimg_yoffset - bch[i] + 1, barWidth, bch[i], surface, xLeft, yTop)
-	
+
 						if capHeight > 0 and cch[i] > 0 then
-							ytop = y - cch[i] - capHeight - capSpace
-							self.fgImg:blitClip(xLeft - xshift, hh - self.fgimg_yoffset - cch[i] - capHeight - capSpace , barWidth, capHeight, surface, xLeft, ytop)
+							yTop = y - cch[i] - capHeight - capSpace
+							self.fgImg:blitClip(xLeft - xshift, hh - self.fgimg_yoffset - cch[i] - capHeight - capSpace ,
+												barWidth, capHeight, surface,
+												xLeft, yTop)
 						end
 					end
 				else
-					if self.turbine then
+					if self.settings.turbine then
 						surface:filledRectangle(
 						x + (k * barSize),
 						self.yCT - (bch[i]/2),
@@ -331,6 +238,87 @@ function _drawBins(self, surface, bins, ch, x, y_in, barsInBin, barWidth, barSpa
 	end
 	return nz
 end
+
+function draw(self, surface)
+-- Black background instead of image
+--	self.bgImg:blit(surface, self:getBounds())
+	local x, y, w, h = self:getBounds()
+
+	-- Avoid calling this more than once as it's not necessary
+	if not self.backgroundDrawn then
+		surface:filledRectangle(x, y, x + w, y + h, self.bgCol)
+		self.backgroundDrawn = true
+	end
+	if self.bgImg ~= nil then
+--		self.bgImg:blit(surface, x, y, w, h, 0, 0)
+--		self.bgImg:blitClip(0, 0, w, h, surface, x, y)
+--		self.bgImg:blitAlpha(surface, x, y, self.bgAlpha)
+		self.bgImg:blit(surface, x, y)
+	end
+
+	local bins = { {}, {} }
+
+	bins[1], bins[2] = vis:spectrum()
+
+	local nz1 = _drawBins(
+		self, surface, bins, 1, self.x1, self.y, self.barsInBin[1],
+		self.barWidth[1], self.barSpace[1], self.binSpace[1],
+		self.barHeightMulti[1], self.settings.capHeight[1], self.settings.capSpace[1]
+	)
+	local nz2 = _drawBins(
+		self, surface, bins, 2, self.x2, self.y, self.barsInBin[2],
+		self.barWidth[2], self.barSpace[2], self.binSpace[2],
+		self.barHeightMulti[2], self.settings.capHeight[2], self.settings.capSpace[2]
+	)
+
+	-- simulate draw analyzer baseline only if playing,
+	-- by not drawing baseline if volume is 0
+	-- good enough
+	if self.settings.baselineAlways or ((nz1 or nz2) and self.settings.baselineOn) then
+		if self.fgImg ~= nil then
+			if self.settings.turbine then
+				self.fgImg:blitClip(self.x1 - x, (h/2) - self.halfblH, self.xSpan, self.blH,
+									surface,
+									self.x1, self.yCT - self.halfblH)
+				self.fgImg:blitClip(self.x2 - x, (h/2) - self.halfblH, self.xSpan, self.blH,
+									surface,
+									self.x2, self.yCT - self.halfblH)
+			else
+				self.fgImg:blitClip(self.x1 - x, h - self.fgimg_yoffset - self.blH, self.xSpan, self.blH,
+									surface,
+									self.x1, self.y - self.fgimg_yoffset - self.blH)
+				self.fgImg:blitClip(self.x2 - x, h - self.fgimg_yoffset - self.blH, self.xSpan, self.blH,
+									surface,
+									self.x2, self.y - self.fgimg_yoffset - self.blH)
+			end
+		else
+			if self.settings.turbine then
+				surface:filledRectangle(self.x1, self.yCT - self.halfblH, self.x1 + self.xSpan ,
+										self.yCT + self.blH - self.halfblH, self.barColor)
+				surface:filledRectangle(self.x2, self.yCT - self.halfblH, self.x2 + self.xSpan ,
+										self.yCT + self.blH - self.halfblH, self.barColor)
+			else
+				surface:filledRectangle(self.x1, self.y - self.blH - self.fgimg_yoffset,
+										self.x1 + self.xSpan, self.y - self.fgimg_yoffset, self.barColor)
+				surface:filledRectangle(self.x2, self.y - self.blH - self.fgimg_yoffset,
+										self.x2 + self.xSpan, self.y - self.fgimg_yoffset, self.barColor)
+			end
+		end
+	end
+	if self.countDown then
+		self.counter = self.counter - 1
+		if self.counter < 1 then
+			visImage:spChange('force', 'force')
+			self:_layout()
+		end
+	end
+end
+
+function twiddle(self)
+	visImage:spChange('force', 'force')
+	self:_layout()
+end
+
 --[[
 
 =head1 LICENSE
@@ -339,7 +327,7 @@ Copyright 2010 Logitech. All Rights Reserved.
 
 This file is licensed under BSD. Please see the LICENSE file for details.
 
-Copyright 2023  additions: Blaise Dias 
+Copyright 2023  additions: Blaise Dias
 =cut
 --]]
 
