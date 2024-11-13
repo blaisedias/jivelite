3706582381cea6ec28acbbfca826bfffd03f1443
Blaise Dias <blaise.dias@datacore.com>
2024-11-13 21:28:05 +0000

    chore: fresh install defaults and userpath
    Make userdir configurable at build time
    set it to visu-4 for this brannc
    
    default bar format to to 1-15-0-5 for spectrum meter
    default to Chevron White Orange for vu meter

--- share/jive/applets/Visualiser/VisualiserMeta.lua
+++ share/jive/applets/Visualiser/VisualiserMeta.lua
@@ -64,7 +64,7 @@ function defaultSettings(_)
             backlitAlpha=80,
             decayAlpha=160,
             capsOn=true,
-            barsFormat="2-1-3-6",
+            barsFormat="1-15-0-5",
             barFormats  = {
                 ["2-1-3-6"]= {barsInBin=2, barWidth=1, barSpace=3, binSpace=6},
                 ["2-2-3-4"]= {barsInBin=2, barWidth=2, barSpace=3, binSpace=4},
