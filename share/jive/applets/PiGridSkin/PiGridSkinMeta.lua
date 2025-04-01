aca6d35dcc3a271ab709f2b280bb615b0f546d9a
Blaise Dias <diasblaise@gmail.com>
2025-04-01 09:25:57 +0100

    chore: Joggler: add support to scale UI up
    Scale UI images on demand to match the display resolution.
    Add module JogglerImageScaler to support this scaling.
    
    Add support functions to Joggler + PiGrid skins for scaling decisions
    - scaleTextValue return scaled values of text to match display
    - scaleGridTextValue return scaled values of text to match display and
      grid layoput requirements
    - scaleImageValue return scaled values of images to match display
    - getJogglerSkinParams returns a table of values scaled to match the display
      - icon thumb size
      - five item height - menu spacing and selection graphics
      - relative path for scaled image storage and access
    - getGridSkinParams returns a table of values scaled to match the display for grid layout
      - icon thumb size
      - grid item height - (Grid skins only) menu spacing and selection graphics
      - relative path for scaled image storage and access
    - scaleUIImages - scales all required UI images to match parameters
    
    The functions above make specific decisions for portrait mode of 720x1280 only,
    other portrait mode resolutions are ignored.
    
    Replace most but not all font size and associated height hard coded values
    with values scaled to match the display.
    
    For desktop add skin entries for 1280x1024 and 1920x1080 resolutions.
    
    FIX: size of art work for NowPlaying views with art and text.
    FIX: misordered retrieval of graphics.
    
    functions getJogglerSkinParams and getGridSkinParams create tables with
    values scaled up from base JogglerSkin values.
    The values in these tables can be overwritten using tables of similar
    shape defined in <user directory>/Joggler.json

--- share/jive/applets/PiGridSkin/PiGridSkinMeta.lua
+++ share/jive/applets/PiGridSkin/PiGridSkinMeta.lua
@@ -51,6 +51,8 @@ function registerApplet(self)
 		jiveMain:registerSkin(self:string("PIGRID_SKIN_1480_320"), "PiGridSkin", "skin1480x320", "PiGridSkin_1480x320")
 		jiveMain:registerSkin(self:string("PIGRID_SKIN_1600_720"), "PiGridSkin", "skin1600x720", "PiGridSkin_1600x720")
 		jiveMain:registerSkin(self:string("PIGRID_SKIN_720_1280"), "PiGridSkin", "skin720x1280", "PiGridSkin_720x1280")
+		jiveMain:registerSkin(self:string("PIGRID_SKIN_1280_1024"), "PiGridSkin", "skin1280x1024", "PiGridSkin_1280x1024")
+		jiveMain:registerSkin(self:string("PIGRID_SKIN_1920_1080"), "PiGridSkin", "skin1920x1080", "PiGridSkin_1920x1080")
 		local screen_width = tonumber(os.getenv('JL_SCREEN_WIDTH') or 0)
 		local screen_height = tonumber(os.getenv('JL_SCREEN_HEIGHT') or 0)
 		-- this skin only really works in landscape mode with a decent ratio of > 1.3
