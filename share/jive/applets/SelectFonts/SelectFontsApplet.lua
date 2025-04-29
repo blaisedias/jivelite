3400710adf12ead9b3c23d158ca28101b6302c61
Blaise Dias <diasblaise@gmail.com>
2025-04-29 13:05:37 +0100

    fix: reflect default font selection in UI
    If no font has been selected or the currently
    selected font does not exist in the hard coded
    database, inidcate FreeSans as the selected
    font.

--- share/jive/applets/SelectFonts/SelectFontsApplet.lua
+++ share/jive/applets/SelectFonts/SelectFontsApplet.lua
@@ -18,26 +18,26 @@ SelectFontsApplet overrides the following methods:
 
 
 -- stuff we use
-local pairs, type = pairs, type
+local pairs = pairs
 
-local table           = require("table")
+--local table           = require("table")
 
 local oo              = require("loop.simple")
 
 local Applet          = require("jive.Applet")
 local RadioButton     = require("jive.ui.RadioButton")
 local RadioGroup      = require("jive.ui.RadioGroup")
-local Checkbox        = require("jive.ui.Checkbox")
-local System          = require("jive.System")
-local debug           = require("jive.utils.debug")
+--local Checkbox        = require("jive.ui.Checkbox")
+--local System          = require("jive.System")
+--local debug           = require("jive.utils.debug")
 local log			  = require("jive.utils.log").logger("applet.SelectFonts")
 local SimpleMenu      = require("jive.ui.SimpleMenu")
 local Window          = require("jive.ui.Window")
 local Framework       = require("jive.ui.Framework")
 local Font            = require("jive.ui.Font")
-local Timer           = require("jive.ui.Timer")
+--local Timer           = require("jive.ui.Timer")
 local JiveMain        = jiveMain
-local appletManager   = appletManager
+--local appletManager   = appletManager
 
 
 module(..., Framework.constants)
@@ -56,14 +56,14 @@ local fontsdb = {
 	Glory={ regular="Glory/Glory-Regular", bold="Glory/Glory-SemiBold", },
 	K2D={ regular="K2D/K2D-Regular", bold="K2D/K2D-SemiBold", },
 	MontserratItalic={
-        regular="Montserrat/static/Montserrat-Italic",
-        bold="Montserrat/static/Montserrat-SemiBoldItalic",
-    },
+		regular="Montserrat/static/Montserrat-Italic",
+		bold="Montserrat/static/Montserrat-SemiBoldItalic",
+	},
 	MontserratMedium={ regular="Montserrat/static/Montserrat-Medium", bold="Montserrat/static/Montserrat-SemiBold", },
 	MontserratRegular={ regular="Montserrat/static/Montserrat-Regular", bold="Montserrat/static/Montserrat-Bold", },
 --	MontserratVariable={
---        regular="Montserrat/Montserrat-VariableFont_wght",
---        bold="Montserrat/Montserrat-Italic-VariableFont_wght"
+--		regular="Montserrat/Montserrat-VariableFont_wght",
+--		bold="Montserrat/Montserrat-Italic-VariableFont_wght"
  --   },
 	MuseoModerno={ regular="MuseoModerno/MuseoModerno-Regular", bold="MuseoModerno/MuseoModerno-SemiBold", },
 	Nunito={ regular="Nunito/Nunito-Regular", bold="Nunito/Nunito-Bold", },
@@ -71,15 +71,15 @@ local fontsdb = {
 	Rajdhani={ regular="Rajdhani/Rajdhani-Regular", bold="Rajdhani/Rajdhani-SemiBold", },
 	SmoochSans={ regular="Smooch Sans/SmoochSans-Regular", bold="Smooch Sans/SmoochSans-SemiBold", },
 	Yanone_Kafeesatz={
-        regular="Yanone_Kafeesatz/YanoneKaffeesatz-Regular",
-        bold="Yanone_Kafeesatz/YanoneKaffeesatz-SemiBold",
-    },
+		regular="Yanone_Kafeesatz/YanoneKaffeesatz-Regular",
+		bold="Yanone_Kafeesatz/YanoneKaffeesatz-SemiBold",
+	},
 	Antonio={ regular="Antonio/static/Antonio-Regular", bold="Antonio/static/Antonio-SemiBold", },
 	Oswald={ regular="Oswald/static/Oswald-Regular", bold="Oswald/static/Oswald-SemiBold", },
-    Roboto= { regular="Roboto/static/Roboto-Regular", bold="Roboto/static/Roboto-SemiBold" },
-    RobotoItalic= { regular="Roboto/static/Roboto-Italic", bold="Roboto/static/Roboto-SemiBoldItalic" },
-    RobotoSemiCondensed= { regular="Roboto/static/Roboto_SemiCondensed-Regular", bold="Roboto/static/Roboto_SemiCondensed-SemiBold" },
-    RobotoCondensed= { regular="Roboto/static/Roboto_Condensed-Regular", bold="Roboto/static/Roboto_Condensed-SemiBold" },
+	Roboto= { regular="Roboto/static/Roboto-Regular", bold="Roboto/static/Roboto-SemiBold" },
+	RobotoItalic= { regular="Roboto/static/Roboto-Italic", bold="Roboto/static/Roboto-SemiBoldItalic" },
+	RobotoSemiCondensed= { regular="Roboto/static/Roboto_SemiCondensed-Regular", bold="Roboto/static/Roboto_SemiCondensed-SemiBold" },
+	RobotoCondensed= { regular="Roboto/static/Roboto_Condensed-Regular", bold="Roboto/static/Roboto_Condensed-SemiBold" },
 }
 
 function selectFontsEntryPoint(self, menuItem)
@@ -99,7 +99,20 @@ function selectFonts(self, title)
 
 	local group = RadioGroup()
 	local current = self:getSettings()['name']
+	local currentFontExists = false
 	log:debug("current ", current)
+	for name, _ in pairs(fontsdb) do
+		currentFontExists = current == name
+		if currentFontExists then
+			break
+		end
+	end
+
+	if not currentFontExists then
+		-- select the default typeface is none is selected or the selected
+		-- typeface does not exist
+		current = 'FreeSans'
+	end
 
 	-- add skins
 	for name, v in pairs(fontsdb) do
@@ -107,8 +120,8 @@ function selectFonts(self, title)
 			text = name,
 			style = 'item_choice',
 			check = RadioButton(
-				"radio", 
-				group, 
+				"radio",
+				group,
 				function()
 					local settings = self:getSettings()
 					settings['regular'] = v['regular']
