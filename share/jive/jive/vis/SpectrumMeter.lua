a9b0fecb0ddaa8cd6d3c67a9597ddf17d689a2a4
Blaise Dias <diasblaise@gmail.com>
2025-06-14 18:16:33 +0100

    chore: spectrum meter: use full height
    - remove total bar height reduction
    - correctly reduce total bar height by capHeight + capSpace
      instead of twice that value
      the number was double when turbine mode was added,
      however capHeight and capSpace was subsequently halved
      when rendering ech tip
    - remove adjustments that force bar increments to be round
      numbers that when multiplied by 31 fit inside the available height
      This means that each level increment may not be identical,
      however the difference will not be noticeable.

--- share/jive/jive/vis/SpectrumMeter.lua
+++ share/jive/jive/vis/SpectrumMeter.lua
@@ -128,12 +128,12 @@ function _layout(self)
 --
 --	barHeight[1] = h - t - b - self.spparms.capHeight[1] - self.spparms.capSpace[1]
 --	barHeight[2] = h - t - b - self.spparms.capHeight[2] - self.spparms.capSpace[2]
-	local barHeight = h - t - b - ((self.spparms.capHeight + self.spparms.capSpace)*2)
-	-- doubly ensure no out of bounds scribbling
-	barHeight = barHeight - 4
-	if barHeight > 31 then
-		barHeight = math.floor(barHeight/31) * 31
-	end
+	local barHeight = h - t - b - (self.spparms.capHeight + self.spparms.capSpace)
+--	-- doubly ensure no out of bounds scribbling
+--	barHeight = barHeight - 4
+--	if barHeight > 31 then
+--		barHeight = math.floor(barHeight/31) * 31
+--	end
 
 	-- max bin value from C code is 31
 --	self.barHeightMulti = {}
@@ -253,10 +253,10 @@ local function _drawBins(surface, bch, params)
 	local dy, yA, yB
 
 	for i = 1, #bch do
-		bch[i] = bch[i] * barHeightMulti
+		bch[i] = math.ceil(bch[i] * barHeightMulti)
 
 		if cch[i] > 0 then
-			cch[i] = cch[i] - barHeightMulti
+			cch[i] = math.ceil(cch[i] - barHeightMulti)
 			if cch[i] < 0 then
 				cch[i] = 0
 			end
@@ -390,10 +390,10 @@ local function _drawTurbineBins(surface, bch, params)
 	local dyFill, yT1, yT2, yB1, yB2, xF
 
 	for i = 1, #bch do
-		bch[i] = bch[i] * barHeightMulti
+		bch[i] = math.ceil(bch[i] * barHeightMulti)
 
 		if cch[i] > 0 then
-			cch[i] = cch[i] - barHeightMulti
+			cch[i] = math.ceil(cch[i] - barHeightMulti)
 			if cch[i] < 0 then
 				cch[i] = 0
 			end
